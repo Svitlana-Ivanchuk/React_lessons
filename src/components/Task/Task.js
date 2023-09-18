c8a205b343d0cea1d21496764eaddeb6de8f33cb
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-09-18 15:49:28 +0300

    add statusFilter

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -6,17 +6,24 @@ import {
   CheckboxStyle,
 } from './Task.styled';
 import { useDispatch } from 'react-redux';
-import { deleteTask } from 'redux/actions';
+import { deleteTask, toggleCompleted } from 'redux/actions';
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
   const handleDelete = () => {
     dispatch(deleteTask(task.id));
   };
+  const handleToggle = () => {
+    dispatch(toggleCompleted(task.id));
+  };
 
   return (
     <TaskStyle>
-      <CheckboxStyle type="checkbox" checked={task.completed} />
+      <CheckboxStyle
+        type="checkbox"
+        checked={task.completed}
+        onChange={handleToggle}
+      />
       <TextStyle>{task.text}</TextStyle>
       <TaskButtonStyle type="button" onClick={handleDelete}>
         <MdClose size={24} />
