8da8c898e52c849599ba989b2b1a66fc83624986
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-09-20 15:48:36 +0300

    finished todo toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,20 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
 
-const enhancer = devToolsEnhancer();
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
 
-export const store = createStore(rootReducer, enhancer);
+//redux
+//import { createStore } from 'redux';
+//import { devToolsEnhancer } from '@redux-devtools/extension';
+//import { rootReducer } from './reducer';
+//import { filtersReducer, tasksReducer } from './reducer';
+
+//const enhancer = devToolsEnhancer();
+
+//export const store = createStore(rootReducer, enhancer);
